c65a877aade64af997b9fbe78074c176e27bd096
Facundo779 <facugrb@gmail.com>
2023-01-22 11:49:50 -0300

    guardarDatos funciona para multiples inputs

--- PrimerProyectoAngularFacu/src/app/elemento/section/subElementos/experiencia/experiencia.component.ts
+++ PrimerProyectoAngularFacu/src/app/elemento/section/subElementos/experiencia/experiencia.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit, TemplateRef, QueryList, ElementRef, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
@@ -16,11 +17,13 @@ export class ExperienciaComponent implements OnInit, AfterViewInit {
   @ViewChild('inputLT') entradaLT!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChild('inputDT') entradaDT!: QueryList<ElementRef<HTMLInputElement>>;
 
-  guardarDatos(event:Event):void{
+  guardarDatos(varLT:any, varDT:any):void{
     //console.log('click');
-    this.lugarDeTrabajo.push((<HTMLInputElement>event.target).value); 
+    this.lugarDeTrabajo.push(varLT); 
+    this.descripcionTarea.push(varDT); 
     //this.descripcionTarea.push(this.entradaDT); 
     console.log(this.lugarDeTrabajo);
+    console.log(this.descripcionTarea);
   }
 
 

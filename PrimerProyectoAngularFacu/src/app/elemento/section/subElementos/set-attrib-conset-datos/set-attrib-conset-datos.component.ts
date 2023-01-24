2d48f82d2d97ac79005d59239d96f00009bc1311
Facundo779 <facugrb@gmail.com>
2023-01-24 19:47:58 -0300

    setAttrib con setDatos funciona

--- PrimerProyectoAngularFacu/src/app/elemento/section/subElementos/set-attrib-conset-datos/set-attrib-conset-datos.component.ts
+++ PrimerProyectoAngularFacu/src/app/elemento/section/subElementos/set-attrib-conset-datos/set-attrib-conset-datos.component.ts
@@ -9,76 +9,93 @@ export class SetAttribConsetDatosComponent implements OnInit, AfterViewInit{
   
   lugarDeTrabajoDos: any[];
   descripcionTareaDos: any[];
-  nameLiTemp: string[];
-  namebLiTemp: string[];
+  tituloEmpleoDos: any[];
   idParrafoTemp: string[];
   arr: string[];
   nuevoName: string;
   nuevoNameB: string;
   nuevoId: string;
-  iterante: number;
+  iteranteDos: number;
+  iteranteHTMLDos: number;
   nuevoNameprrfLT: string;
   nuevoNameprrfDT: string;
+  nuevoNameprrfTE: string;
   restInputLTDos: string;
   restInputDTDos: string;
+  restInputTEDos: string;
+  datosGuardados: boolean;
 
   @ViewChild('nuevaSeccion') btnNewWork:any;
   @ViewChild('temBaseDos') newTemp!: TemplateRef<any>;
   @ViewChild('temBaseDos', {read:ViewContainerRef}) newContTemp!: ViewContainerRef;
   @ViewChildren('prrfLT') conjprrfLT!: QueryList<ElementRef<HTMLParagraphElement>>;
   @ViewChildren('prrfDT') conjprrfDT!: QueryList<ElementRef<HTMLParagraphElement>>;
+  @ViewChildren('prrfTE') conjprrfTE!: QueryList<ElementRef<HTMLParagraphElement>>;
   @ViewChild('inputLTDos') entradaLTDos!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChild('inputDTDos') entradaDTDos!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChildren('divTemp') divTemp!: QueryList<ElementRef<HTMLDivElement>>;
 
   nuevoTempYseteoAtribDos():void{
     this.newContTemp.createEmbeddedView(this.newTemp);
-    this.iterante = this.iterante + 1; 
-    this.nuevoId = "divTemp" + this.iterante;
-    this.nuevoName = "divTemp" + this.iterante; 
-    this.nuevoNameB = "divTemp" + this.iterante; 
-    this.nuevoNameprrfLT = "varLT" + this.iterante; 
-    this.nuevoNameprrfDT = "varDT" + this.iterante; 
+    this.iteranteDos = this.iteranteDos + 1; 
+    this.iteranteHTMLDos = this.iteranteHTMLDos + 1; 
+    this.nuevoId = "divTemp" + this.iteranteDos;
+    this.nuevoName = "divTemp" + this.iteranteDos; 
+    this.nuevoNameB = "divTemp" + this.iteranteDos; 
+    this.nuevoNameprrfLT = "varLT" + this.iteranteDos; 
+    this.nuevoNameprrfDT = "varDT" + this.iteranteDos; 
+    this.nuevoNameprrfTE = "varTE" + this.iteranteDos; 
     setTimeout(()=>{
       this.divTemp.last.nativeElement.setAttribute('id',`${this.nuevoId}`);
       this.divTemp.last.nativeElement.setAttribute('name',`${this.nuevoName}`);
       this.divTemp.last.nativeElement.setAttribute('nameb',`${this.nuevoNameB}`);
       this.conjprrfLT.last.nativeElement.setAttribute('namelt',`${this.nuevoNameprrfLT}`);
       this.conjprrfDT.last.nativeElement.setAttribute('namedt',`${this.nuevoNameprrfDT}`);
+      this.conjprrfTE.last.nativeElement.setAttribute('namete',`${this.nuevoNameprrfTE}`);
+      
+      this.conjprrfLT.last.nativeElement.textContent = this.lugarDeTrabajoDos[-1 + this.iteranteDos];
+      this.conjprrfDT.last.nativeElement.textContent = this.descripcionTareaDos[-1 + this.iteranteDos];
+      this.conjprrfTE.last.nativeElement.textContent = this.tituloEmpleoDos[-1 + this.iteranteDos];
+
     }, 100);
   }
 
-  guardarDatosDos(varLTDos:any, varDTDos:any):void{
+  guardarDatosDos(varTEDos:any, varLTDos:any, varDTDos:any):void{
     //console.log('click');
     if(varLTDos&&varDTDos!==""){
       this.nuevoTempYseteoAtribDos();
       this.lugarDeTrabajoDos.push(varLTDos); 
       this.descripcionTareaDos.push(varDTDos); 
+      this.tituloEmpleoDos.push(varTEDos); 
       this.restInputLTDos = ""; 
       this.restInputDTDos = "";
-    }else{
+      this.restInputTEDos = "";
+      this.datosGuardados = true;
       
+    }else{
+      this.datosGuardados = false;
     }
     //this.descripcionTarea.push(this.entradaDT); 
-    console.log(this.lugarDeTrabajoDos);
-    console.log(this.descripcionTareaDos);
   }
 
   constructor() {
     this.lugarDeTrabajoDos = [];
     this.descripcionTareaDos = [];
-    this.nameLiTemp = [];
-    this.namebLiTemp = [];
+    this.tituloEmpleoDos = [];
     this.idParrafoTemp = [];
     this.arr = [];
     this.nuevoId="";
     this.nuevoName="";
     this.nuevoNameB="";
-    this.iterante = 0;
+    this.iteranteDos = 0;
+    this.iteranteHTMLDos = -1;
     this.nuevoNameprrfLT ="";
     this.nuevoNameprrfDT ="";
+    this.nuevoNameprrfTE ="";
     this.restInputLTDos ="";
     this.restInputDTDos ="";
+    this.restInputTEDos ="";
+    this.datosGuardados = false;
    }
 
   ngOnInit(): void {
